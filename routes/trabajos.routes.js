d5a85fac84aedb53a4248e722a35ddc1af81adbd
FClavijo00 <fjclavijo00@gmail.com>
2026-07-25 22:17:44 +0200

    obtenerUltimosTrabajos

--- routes/trabajos.routes.js
+++ routes/trabajos.routes.js
@@ -4,4 +4,28 @@ const express = require("express");
 const router = express.Router();
 const db = require("../src/db"); // Importa tu archivo de conexión
 
+// Función para traer los últimos trabajos realizados
+// Endpoint: GET /api/trabajos/
+router.get("/obtenerUltimosTrabajos", async (req, res) => {
+    try {
+        const sql = 
+        `
+        SELECT TR.*, P.nombre_parcela, P.apodo_parcela, TT.nombre
+        FROM trabajos_realizados TR
+        INNER JOIN parcelas P
+        ON P.id = TR.parcela_id
+        INNER JOIN tipos_trabajos TT
+        ON TT.id = TR.tipo_trabajo_id
+        ORDER BY TR.fecha_trabajo DESC
+        LIMIT 5
+        `;
+        const result = await db.query(sql);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error("Error al obtener los últimos trabajos.", err);
+        res.status(500).json({ error: "Error interno del servidor." });
+    }
+});
+
+
 module.exports = router;
