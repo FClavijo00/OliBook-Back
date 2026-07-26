2567cefec002e4370c8e5843fd5c302bdcfe98a3
FClavijo00 <fjclavijo00@gmail.com>
2026-07-26 19:43:00 +0200

    obtenerParcelas 1.0

--- routes/parcelas.routes.js
+++ routes/parcelas.routes.js
@@ -4,4 +4,16 @@ const express = require("express");
 const router = express.Router();
 const db = require("../src/db"); // Importa tu archivo de conexión
 
+router.post("/obtenerParcelas", async (req, res) => {
+    try {
+        const { userID } = req.body;
+        const sql = "SELECT * FROM parcelas WHERE user_id = $1";
+        const result = await db.query(sql, [userID]);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error("Error al obtener las parcelas:", error);
+        res.status(500).json({ error: "Error interno del servidor." });
+    }
+});
+
 module.exports = router;
