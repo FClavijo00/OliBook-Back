fda22ba6ba80a1e7ae8a3ec493cfa2c27ca3e9d7
FClavijo00 <fjclavijo00@gmail.com>
2026-07-26 18:30:19 +0200

    obtenerEmpresaUsuario actualizado

--- routes/empresas.routes.js
+++ routes/empresas.routes.js
@@ -4,15 +4,17 @@ const express = require("express");
 const router = express.Router();
 const db = require("../src/db"); // Importa tu archivo de conexión
 
-// Endpoint: GET /api/users/obtenerEmpresaActual
-router.get("/obtenerEmpresaActual", async (req, res) => {
+// Endpoint: GET /api/users/obtenerEmpresaUsuario
+router.post("/obtenerEmpresaUsuario", async (req, res) => {
     try {
+        const { id } = req.body;
         const sql = 
-        `SELECT E.* FROM empresas E
+        `SELECT E.nombre_empresa, E.codigo_empresa 
+        FROM empresas E
         INNER JOIN users U
         ON U.empresa_id = E.id 
         WHERE U.id = $1`;
-        const result = await db.query(sql, [req.user.id]);
+        const result = await db.query(sql, [id]);
         res.status(200).json(result.rows[0]);
     } catch (err) {
         console.error("Error al obtener la empresa actual:", err);
